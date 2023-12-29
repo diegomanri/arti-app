422f9e4624f03efa0d4113b6e40f0cb04ad3408e
Diego Manrique <diegomanri@gmail.com>
2023-12-28 22:40:46 -0600

    Infra launch 1

--- terraform/ssm-and-secrets.tf
+++ terraform/ssm-and-secrets.tf
@@ -1,39 +1,39 @@
-# Random password generator resource
-resource "random_password" "rds_pass" {
-  length  = 16
-  special = true
-}
+# # Random password generator resource
+# resource "random_password" "rds_pass" {
+#   length  = 16
+#   special = true
+# }
 
-# AWS Secrets Manager
-#RDS Password
-resource "aws_secretsmanager_secret" "rds_password" {
-  name = "rds_password"
-}
+# # AWS Secrets Manager
+# #RDS Password
+# resource "aws_secretsmanager_secret" "rds_password" {
+#   name = "rds_password"
+# }
 
-resource "aws_secretsmanager_secret_version" "rds_password" {
-  secret_id     = aws_secretsmanager_secret.rds_password.id
-  secret_string = random_password.rds_pass.result
-}
+# resource "aws_secretsmanager_secret_version" "rds_password" {
+#   secret_id     = aws_secretsmanager_secret.rds_password.id
+#   secret_string = random_password.rds_pass.result
+# }
 
-# AWS Parameter Store
-resource "aws_ssm_parameter" "rds_username" {
-  name  = "/finarticles/rds_username"
-  type  = "String"
-  value = "produser"
-}
+# # AWS Parameter Store
+# resource "aws_ssm_parameter" "rds_username" {
+#   name  = "/finarticles/rds_username"
+#   type  = "String"
+#   value = "produser"
+# }
 
-resource "aws_ssm_parameter" "rds_host" {
-  name = "/finarticles/rds_host"
-  type = "String"
-  #value = module.db.db_instance_address
-  value = aws_db_instance.artiapp_db.address
-}
+# resource "aws_ssm_parameter" "rds_host" {
+#   name = "/finarticles/rds_host"
+#   type = "String"
+#   #value = module.db.db_instance_address
+#   value = aws_db_instance.artiapp_db.address
+# }
 
-resource "aws_ssm_parameter" "rds_db_name" {
-  name  = "/finarticles/rds_db_name"
-  type  = "String"
-  value = "artiappdb"
-}
+# resource "aws_ssm_parameter" "rds_db_name" {
+#   name  = "/finarticles/rds_db_name"
+#   type  = "String"
+#   value = "artiappdb"
+# }
 
 # GHCR Token
 resource "aws_secretsmanager_secret" "ghcr_token" {
